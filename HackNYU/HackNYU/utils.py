baa7bd173a79a6152e6f42de516744ab52579b49
andy <at2706@nyu.edu>
2017-02-18 10:13:26 -0500

    Insurance Data

--- HackNYU/HackNYU/utils.py
+++ HackNYU/HackNYU/utils.py
@@ -1,6 +1,6 @@
 import csv
 from django.db import transaction
-from .models import Hospital, HospitalAddress
+from .models import Hospital, HospitalAddress, Insurance
 
 
 def add():
@@ -12,3 +12,15 @@ def add():
             hospital = Hospital.objects.create(name=str(row[1]).title(), phone_number=int(row[7]))
             HospitalAddress.objects.create(hospital=hospital, address1=str(
                 row[2]).title(), city=str(row[3]).title(), state=str(row[4]), zipcode=int(row[5]))
+
+
+def add2():
+    f = open('us_companies.csv')
+    csvfile = csv.reader(f)
+    next(csvfile, None)
+
+    with transaction.atomic():
+        for row in csvfile:
+            types = row[17].split(',')
+            if 'Health/Healthcare' in types:
+                Insurance.objects.create(name=row[1])
