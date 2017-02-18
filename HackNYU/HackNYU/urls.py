0d5f73e7408c31321e72889e5850dcb0826fc47a
andy <at2706@nyu.edu>
2017-02-17 23:23:46 -0500

    Index View

--- HackNYU/HackNYU/urls.py
+++ HackNYU/HackNYU/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+from . import views
 
 urlpatterns = [
+    # Django Default User Authentication
+    # https://docs.djangoproject.com/en/1.10/topics/auth/default/
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 
+    url(r'^$', views.IndexView.as_view(), name='index'),
 ]
