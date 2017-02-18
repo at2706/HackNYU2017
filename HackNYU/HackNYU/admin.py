f53e48b25a7af52a49b4b5f59f214335b60ce414
andy <at2706@nyu.edu>
2017-02-18 12:20:39 -0500

    admin

--- HackNYU/HackNYU/admin.py
+++ HackNYU/HackNYU/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
+admin.site.register(models.Patient)
+admin.site.register(models.Doctor)
+admin.site.register(models.UserAddress)
+admin.site.register(models.Doctor)
 admin.site.register(models.Hospital)
 admin.site.register(models.HospitalAddress)
+admin.site.register(models.MedicalRecord)
+admin.site.register(models.LabReport)
+admin.site.register(models.inpatient)
+admin.site.register(models.outpatient)
+admin.site.register(models.Room)
--- HackNYU/HackNYUProject/settings.py
+++ HackNYU/HackNYUProject/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'HackNYUProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'xdmemes',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
